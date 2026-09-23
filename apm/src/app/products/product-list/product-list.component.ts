6bd4799c03fa0b730a3c0d14af4bf683c9ed2873
Varun Gupta <varun.gupta@outlook.in>
2026-09-23 10:09:25 +0530

    Added review service, use declerative approach for http call with async pipe

--- apm/src/app/products/product-list/product-list.component.ts
+++ apm/src/app/products/product-list/product-list.component.ts
@@ -1,45 +1,32 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
-import { NgIf, NgFor, NgClass } from '@angular/common';
-import { Product } from '../product';
+import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { catchError, EMPTY, Subscription, tap } from 'rxjs';
+import { catchError, EMPTY, tap } from 'rxjs';
 
 @Component({
     selector: 'pm-product-list',
     templateUrl: './product-list.component.html',
     standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
+  imports: [AsyncPipe, NgIf, NgFor, NgClass, ProductDetailComponent]
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent{
   pageTitle = 'Products';
   errorMessage = '';
-  productSub!: Subscription;
   private productService = inject(ProductService);
   // Products
-  products: Product[] = [];
-
-  // Selected product id to highlight the entry
-  selectedProductId: number = 0;  
-
-
-  ngOnInit(): void {
-    this.productSub = this.productService.getProducts()
+  readonly products$ = this.productService.products$
     .pipe(
       tap(() => console.log('prodouct list - fetched products')),
       catchError(err => {
         this.errorMessage = err;
         return EMPTY;
       })
-    ).subscribe({
-      next: (products) => this.products = products
-    });
-  }
+    );
 
-  ngOnDestroy(): void {
-    this.productSub.unsubscribe();
-  }
+  // Selected product id to highlight the entry
+  selectedProductId: number = 0;  
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from './product';
-import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +14,30 @@ export class ProductService {
   private productsUrl = 'api/products';
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
-  getProducts() : Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
+  private reviewService = inject(ReviewService);
+  readonly products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
       tap(() => console.log('fetched products')),
       catchError(err => this.handleError(err))
     );
-  }
+ 
   getProduct(id: number) : Observable<Product> {
     const url = `${this.productsUrl}/${id}`;
     return this.http.get<Product>(url)
     .pipe(
-      tap(() => console.log(`fetched product id=${id}`)),
+      switchMap(product => this.getProductsWithReviews(product)),
       catchError(err => this.handleError(err))
     );
+  }
+  getProductsWithReviews(product: Product) : Observable<Product> {
+    if(product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+      .pipe(
+        map(reviews => ({...product, reviews} as Product))
+      )
+    } else {
+      return of(product);
+    }
   }
   private handleError(err: HttpErrorResponse) : Observable<never> {
     const formattedMessage = this.errorService.formatError(err);
